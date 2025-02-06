e28519bb95b8ee0a5437b7f567c8fa6e829043e6
mohamed-aymen-ibk <a.azza@ibk.ma>
2025-02-06 13:48:09 +0100

    frontend setup

--- payment-deadline-frontend/src/main.js
+++ payment-deadline-frontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import 'primevue/resources/themes/saga-blue/theme.css';  // Choose the theme you prefer
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import App from "@/App.vue";
+import {createApp} from "vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('DataTable', DataTable);
+app.component('PrimeColumn', Column);
+app.mount('#app');
